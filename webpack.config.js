e008c96c5e8fd182252f8afb2347a3e99905da01
Pushkar DK <pushkar100@gmail.com>
2020-05-01 17:01:19 +0530

    adding different support for different modes in configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path')
 
-module.exports = {
-    mode: 'none',
+module.exports = ({ mode }) => ({
+    mode,
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
-}
+})
