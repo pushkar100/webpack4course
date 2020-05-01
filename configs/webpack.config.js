38cb95c8c98df3c4c689ee40bd727a9c644bcc37
Pushkar DK <pushkar100@gmail.com>
2020-05-02 03:39:29 +0530

    Creating a separate folder for config files (fixes)

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = ({ mode }) => ({
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '../dist') // Needs to be an absolute path
     },
     module: {
         rules: [
